c21976cab6a21a19dd2c7e93e74b40a7350eb766
Lara Sanders <larasanders@gmail.com>
2018-07-26 22:52:56 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -9,8 +9,8 @@ class RepositoriesController < ApplicationController
        @resp = Faraday.get 'https://api.github_search.com/application/vnd.github[.version].param[+json]' do |req|
         req.params['client_id'] = 'Iv1.d181c973798d9df9'
         req.params['client_secret'] = '1a4cec10cdb3be577b31ea94acb208f449724322'
-        req.params['name'] = 'basic-apis-lab-trial'
-        req.params['description'] = 'This is a trial repository. Hopefully it works.'
+        req.params['name'] = params[:name]
+        req.params['query'] = 'Github App'
       end
       body = JSON.parse(@resp.body)
  if @resp.success?
